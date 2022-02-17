04de15de99dc435d1450c3559bd9fc2ef59eec50
Simon Petrac <pfoerdie@gmail.com>
2022-02-17 14:54:20 +0100

    bugfix

--- src/module.geom.js
+++ src/module.geom.js
@@ -33,73 +33,73 @@ exports.GeometryCollection = GeometryCollection;
 exports.equals = function (left, right) {
     assert(isGeometry(left), `${PREFIX}:equals : invalid @param {Geometry} left`, TypeError);
     assert(isGeometry(right), `${PREFIX}:equals : invalid @param {Geometry} right`, TypeError);
-    left.equals(right);
+    return left.equals(right);
 };
 
 exports.disjoint = function (left, right) {
     assert(isGeometry(left), `${PREFIX}:disjoint : invalid @param {Geometry} left`, TypeError);
     assert(isGeometry(right), `${PREFIX}:disjoint : invalid @param {Geometry} right`, TypeError);
-    left.disjoint(right);
+    return left.disjoint(right);
 };
 
 exports.intersects = function (left, right) {
     assert(isGeometry(left), `${PREFIX}:intersects : invalid @param {Geometry} left`, TypeError);
     assert(isGeometry(right), `${PREFIX}:intersects : invalid @param {Geometry} right`, TypeError);
-    left.intersects(right);
+    return left.intersects(right);
 };
 
 exports.touches = function (left, right) {
     assert(isGeometry(left), `${PREFIX}:touches : invalid @param {Geometry} left`, TypeError);
     assert(isGeometry(right), `${PREFIX}:touches : invalid @param {Geometry} right`, TypeError);
-    left.touches(right);
+    return left.touches(right);
 };
 
 exports.meets = function (left, right) {
     assert(isGeometry(left), `${PREFIX}:meets : invalid @param {Geometry} left`, TypeError);
     assert(isGeometry(right), `${PREFIX}:meets : invalid @param {Geometry} right`, TypeError);
-    left.meets(right);
+    return left.meets(right);
 };
 
 exports.contains = function (left, right) {
     assert(isGeometry(left), `${PREFIX}:contains : invalid @param {Geometry} left`, TypeError);
     assert(isGeometry(right), `${PREFIX}:contains : invalid @param {Geometry} right`, TypeError);
-    left.contains(right);
+    return left.contains(right);
 };
 
 exports.overlaps = function (left, right) {
     assert(isGeometry(left), `${PREFIX}:overlaps : invalid @param {Geometry} left`, TypeError);
     assert(isGeometry(right), `${PREFIX}:overlaps : invalid @param {Geometry} right`, TypeError);
-    left.overlaps(right);
+    return left.overlaps(right);
 };
 
 exports.covers = function (left, right) {
     assert(isGeometry(left), `${PREFIX}:covers : invalid @param {Geometry} left`, TypeError);
     assert(isGeometry(right), `${PREFIX}:covers : invalid @param {Geometry} right`, TypeError);
-    left.covers(right);
+    return left.covers(right);
 };
 
 exports.coveredBy = function (left, right) {
     assert(isGeometry(left), `${PREFIX}:coveredBy : invalid @param {Geometry} left`, TypeError);
     assert(isGeometry(right), `${PREFIX}:coveredBy : invalid @param {Geometry} right`, TypeError);
-    left.coveredBy(right);
+    return left.coveredBy(right);
 };
 
 exports.inside = function (left, right) {
     assert(isGeometry(left), `${PREFIX}:inside : invalid @param {Geometry} left`, TypeError);
     assert(isGeometry(right), `${PREFIX}:inside : invalid @param {Geometry} right`, TypeError);
-    left.inside(right);
+    return left.inside(right);
 };
 
 exports.within = function (left, right) {
     assert(isGeometry(left), `${PREFIX}:within : invalid @param {Geometry} left`, TypeError);
     assert(isGeometry(right), `${PREFIX}:within : invalid @param {Geometry} right`, TypeError);
-    left.within(right);
+    return left.within(right);
 };
 
 exports.crosses = function (left, right) {
     assert(isGeometry(left), `${PREFIX}:crosses : invalid @param {Geometry} left`, TypeError);
     assert(isGeometry(right), `${PREFIX}:crosses : invalid @param {Geometry} right`, TypeError);
-    left.crosses(right);
+    return left.crosses(right);
 };
 
 Object.freeze(exports);
